2d85901c62bf46a9a5951fa33fa879e87d8b4b2b
Aditi Maheshwari <aditim210@gmail.com>
2023-11-03 01:30:40 +0530

    create and edit post

--- api/index.js
+++ api/index.js
@@ -1,28 +1,33 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const { json } = require('body-parser');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcrypt');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
+
 const salt = bcrypt.genSaltSync(10);
-const jwt = require('jsonwebtoken')
-const secret = 'hgasdiuj763ehj2h';
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-// set credentials to true to allow cookie
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-
-// parse json from request
 app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://blog:2DbHT4PqEEk9LAMZ@cluster0.bzlq4js.mongodb.net/');
+mongoose.connect('mongodb+srv://blog:<password>@cluster0.bzlq4js.mongodb.net/');
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    // only unique username is allowed. Hence try and catch
     try {
-        // hashed password
-        const userDoc = await User.create({ username, password: bcrypt.hashSync(password, salt), });
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
     } catch (e) {
         console.log(e);
@@ -48,6 +53,86 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.listen(4000)
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        });
+        res.json(postDoc);
+    });
+
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('you are not the author');
+        }
+        await postDoc.update({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        });
+
+        res.json(postDoc);
+    });
+
+});
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({ createdAt: -1 })
+            .limit(20)
+    );
+});
+
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+})
+
+app.listen(4000);
 //
-// blog
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,12 +1,42 @@
-import {Link} from "react-router-dom";
-export default function Header(){
-    return (
-        <header>
-          <Link to="/" className="logo">MyBlog</Link>
-          <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-          </nav>
-        </header>
-      );
+import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from './UserContext'
+export default function Header() {
+  // info about the user should not be stored inside const
+  // it must be in context.js
+  // const [username, setUsername] = useState('');
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, [setUserInfo]);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    })
+    setUserInfo(null);
+  }
+  const username = userInfo?.username;
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (<>
+          <Link to='/create'>Create New Post</Link>
+          <Link onClick={logout}>LogOut</Link>
+        </>)}
+        {!username && (<>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>)}
+      </nav>
+    </header>
+  );
 }
